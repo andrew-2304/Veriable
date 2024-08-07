4d1205d6d5d9293903dfb7276d985964b77e34ef
andrew-2304 <henryostwald23@gmail.com>
2024-08-07 22:51:09 +0300

    Add files via upload

---
+++ Veriable.py
@@ -0,0 +1,5 @@
+number_of_tasks = 12
+number_of_hours = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours/number_of_tasks
+print('Курс:',course_name, ', всего задач:',number_of_tasks, ', затрачено часов:',number_of_hours, ', среднее время выполнения:', time_for_one_task, 'часа.')
